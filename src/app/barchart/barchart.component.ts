aeb11fa304809067b815d3b52ec2dd53b6bc5cad
screen-t <yassinemt08@gmail.com>
2023-07-04 08:58:34 +0100

    creating the bar charts with data

--- src/app/barchart/barchart.component.ts
+++ src/app/barchart/barchart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-barchart',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./barchart.component.css']
 })
 export class BarchartComponent {
+  public data : Array<any> = [
+    {data : [10 , 20 , 30 , 1 , 12 ] , label : "data 1" , backgroundColor: "rgb(102,93,162)" }
+  ] ; 
+  public lables : Array<any> = ["lun" , "mar" , "mer" , "jeu" , "vend"];
+  public type : ChartType = "bar" ; 
+  public options : any = {
+    responsive: true,
+  }
 
 }
