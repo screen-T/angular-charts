af73d5c712cce6f7d11c1329e61627c9e6dd0ca0
screen-t <yassinemt08@gmail.com>
2023-07-04 10:39:07 +0100

    making find all api

--- server/routes/data1.js
+++ server/routes/data1.js
@@ -1,6 +1,7 @@
 const Data1= require ("../models/data1")
 const express= require("express")
-const { response } = require("express")
+const { response } = require("express");
+const data1 = require("../models/data1");
 const router = express.Router() ;
 router.post("/add" , async (req  , resp)=>{
     try {
@@ -18,4 +19,20 @@ catch (error){
     resp.status(400).send(error)
 }
 } )
+
+router.get("/all", async (req , resp)=>{
+    try {
+        console.log("find data")
+        var data= await Data1.find()
+        resp.status(200).send(data)
+    }
+    catch (error)
+    {
+       var status= {
+            message : "error selecting the data" ,
+            error : error  
+        }
+        resp.satus(400).send(status)
+    }
+})
 module.exports = router
